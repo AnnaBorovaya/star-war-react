9b68dd9de540b203b02c44ab262fddb0b03001e8
AnnaBorovaya <anna.kviten11@gmail.com>
2019-08-20 11:01:56 +0300

    some

---
+++ precache-manifest.10b35e7b7cc8f993bc84fe20e4550011.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ca0018715e6c9dfb862ef5c018a2a1e",
+    "url": "/star-war-react/index.html"
+  },
+  {
+    "revision": "06f541f8465903300775",
+    "url": "/star-war-react/static/css/main.fe81e25b.chunk.css"
+  },
+  {
+    "revision": "d53c57f0bf9a4f06dc3d",
+    "url": "/star-war-react/static/js/2.e44e4442.chunk.js"
+  },
+  {
+    "revision": "06f541f8465903300775",
+    "url": "/star-war-react/static/js/main.4a06e63b.chunk.js"
+  },
+  {
+    "revision": "de161acded6f268c5161",
+    "url": "/star-war-react/static/js/runtime~main.3f554244.js"
+  }
+]);
